7e860c1bee8c40736413276030c445159b6e547d
jacobmacwilliams <jacobmacwilliams@gmail.com>
2022-09-09 10:21:20 +0200

    Add create simple pdf examples.

---
+++ pdfcreate/src/test/java/com/example/create/CreateFormTest.java
@@ -0,0 +1,18 @@
+package com.example.create;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class CreateFormTest {
+  /**
+   * Rigorous Test :-)
+   */
+  @Test
+  public void shouldAnswerWithTrue() {
+    assertTrue(true);
+  }
+}
